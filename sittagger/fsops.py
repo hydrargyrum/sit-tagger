5141978d0325aff008fe51b3423efe46a10af589
Hg <dev@indigo.re>
2021-05-19 22:42:16 +0200

    imagewidgets: be able to rename files directly

--- sittagger/fsops.py
+++ sittagger/fsops.py
@@ -12,6 +12,33 @@ def _get_dev(path):
 	return path.lstat().st_dev
 
 
+def _rename_with_thumb(src, dst):
+	if src.startswith(vignette._thumb_path_prefix()):
+		return _os_rename(src, dst)
+
+	tsrc = vignette.try_get_thumbnail(src, 'large')
+	if not tsrc:
+		return _os_rename(src, dst)
+
+	ret = _os_rename(src, dst)
+
+	vignette.put_thumbnail(dst, 'large', tsrc)
+
+	return ret
+
+
+def move_file(old, new, db):
+	old = Path(old).absolute()
+	new = Path(new).absolute()
+
+	if _get_dev(old) != _get_dev(new.parent):
+		raise NotImplementedError()
+
+	_rename_with_thumb(str(old), str(new))
+	with db:
+		db.rename_file(str(old), str(new))
+
+
 def _scan_thumbs(path):
 	ret = {}
 	for sub in path.rglob('*'):
--- sittagger/imagewidgets.py
+++ sittagger/imagewidgets.py
@@ -1,10 +1,16 @@
 
 import os
+from pathlib import Path
 
 from PyQt5.QtCore import QSize, Qt, pyqtSlot as Slot
-from PyQt5.QtGui import QIcon, QPixmap
-from PyQt5.QtWidgets import QListWidget, QListWidgetItem, QListView
+from PyQt5.QtGui import QIcon, QPixmap, QKeySequence
+from PyQt5.QtWidgets import (
+	QListWidget, QListWidgetItem, QListView,
+	QAction, QInputDialog, QLineEdit,
+	QMessageBox,
+)
 
+from .fsops import move_file
 from . import thumbnailmaker
 
 
@@ -26,6 +32,14 @@ class ThumbnailItem(QListWidgetItem):
 	def getPath(self):
 		return self.path
 
+	def getPathObject(self):
+		return Path(self.getPath())
+
+	def _updatePath(self, newpath):
+		self.path = newpath
+		self.setData(Qt.UserRole, self.path)
+		self.setText(Path(newpath).name)
+
 
 class ImageList(QListWidget):
 	def __init__(self, *args, **kwargs):
@@ -33,6 +47,12 @@ class ImageList(QListWidget):
 		self.items = {}
 
 		thumbnailmaker.maker.done.connect(self._thumbnailDone)
+
+		action = QAction(parent=self)
+		action.setShortcut(QKeySequence("F2"))
+		action.setShortcutContext(Qt.WidgetShortcut)
+		action.triggered.connect(self.popRenameSelected)
+		self.addAction(action)
 
 	@Slot(str, str)
 	def _thumbnailDone(self, origpath, thumbpath):
@@ -56,3 +76,40 @@ class ImageList(QListWidget):
 
 	def getFiles(self):
 		return [self.item(i).getPath() for i in range(self.count())]
+
+	@Slot()
+	def popRenameSelected(self):
+		db = self.window().db
+
+		selected = self.selectedItems()
+		if len(selected) != 1:
+			# cannot rename 0 or multiple files
+			return
+
+		current = selected[0].getPathObject().absolute()
+
+		new, ok = QInputDialog.getText(
+			self,
+			self.tr("Rename file"),
+			self.tr("New name for file"),
+			QLineEdit.Normal,
+			current.name
+		)
+
+		if not ok or not new or new == current.name:
+			return
+		if "/" in new:
+			QMessageBox.critical(
+				self,
+				self.tr("Error"),
+				self.tr("New name %r cannot contain '/'") % new,
+			)
+			return
+
+		new = current.with_name(new)
+
+		move_file(current, new, db)
+
+		item = self.items.pop(str(current))
+		item._updatePath(str(new))
+		self.items[str(new)] = item
